48dbe9f98f962d191225646796ac1284aa54b4a2
me <lol@amus.cc>
2023-09-10 14:24:22 -0500

    fixed compiling issue

--- src/hacks/hitchance.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class CEntity;
-class CVector;
-namespace hacks
-{
-	bool CheckHitchance(CEntity* player, const CVector& angle) noexcept;
-}
-
--- src/valve/centity.h
+++ src/valve/centity.h
@@ -206,43 +206,5 @@ public: // entity virtual functions
 	{
 		return memory::Call<float>(this, 439);
 	}
-
-	CVector CalculateSpread(int seed, float inaccuracy, float spread) {
-
-		CEntity* activeWeapon = globals::localPlayer->GetActiveWeapon();
-
-		float recoil_index, r1, r2, r3, r4, s1, c1, s2, c2;
-
-		auto randomSeedFn = reinterpret_cast<void(__cdecl*)(int)>(interfaces::randomSeed);
-		auto randomFloatFn = reinterpret_cast<float(__cdecl*)(float, float)>(interfaces::randomFloat);
-
-		if (!activeWeapon) {
-			return {};
-		}
-
-		int weaponType = activeWeapon->GetWeaponType();
-
-		// Seed random
-		randomSeedFn(seed & 0xff);
-
-		float inaccuracy = activeWeapon->GetInaccuracy();
-		float spread = activeWeapon->GetSpread();
-
-		float r1 = randomFloatFn(0.0f, 1.0f);
-		float r2 = randomFloatFn(0.0f, 3.1415926535897932384f * 2);
-
-		// get needed sine / cosine values.
-		c1 = std::cos(r2);
-		c2 = std::cos(r4);
-		s1 = std::sin(r2);
-		s2 = std::sin(r4);
-
-		// calculate spread vector.
-		return {
-			(c1 * (r1 * inaccuracy)) + (c2 * (r3 * spread)),
-			(s1 * (r1 * inaccuracy)) + (s2 * (r3 * spread)),
-			0.f
-		};
-	}
 	
 };
